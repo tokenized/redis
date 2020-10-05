7afe113a4fc240e9c29628a188ab7f0152a41a14
Scott Barr <scottjbarr@gmail.com>
2020-10-05 15:36:24 +1000

    Fix Sender interface

--- redis.go
+++ redis.go
@@ -2,5 +2,5 @@ package redis
 
 // Sender is an interface that is compatible with the redis.Send(..) function.
 type Sender interface {
-	Send(string, args ...interface{}) error
+	Send(string, ...interface{}) error
 }
